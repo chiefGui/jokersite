37586bb0edc5e4bcfc43c6fabbdbabb3f2b7c2cd
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2015-09-10 23:06:18 -0300

    Appending form when clicking button

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#want-a-discount--button').on('click', function () {
+    $('#want-a-discount--form').toggleClass('is-visible')
+    $('#buttons').toggleClass('is-visible')
+  })
+})

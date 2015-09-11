2d7df3d2ccb0b8756aaadee1da93ca581a631ff8
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2015-09-10 23:11:10 -0300

    fixing shit I did

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#want-a-discount--button').on('click', function () {
-    $('#want-a-discount--form').toggleClass('is-visible')
-    $('#buttons').toggleClass('is-visible')
+    $('#want-a-discount--form').addClass('is-visible')
   })
 })

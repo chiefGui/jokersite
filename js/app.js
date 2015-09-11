4622d2f2b2c5d14028378105e61d90d20361d012
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2015-09-10 23:15:04 -0300

    Updating

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('#want-a-discount--button').on('click', function () {
     $('#want-a-discount--form').addClass('is-visible')
+    $('.bloco-destaque').toggleClass('is-visible')
   })
 })
